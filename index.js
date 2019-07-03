98fa8e346d25fe1a57cf9d572427f070d07b2173
John Azzaro <jazzaro@temple.edu>
2019-07-02 21:26:29 -0700

    element selector

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 'use strict';
 
-class Calculator {                                              /* Calculator class to store our data input...*/
-                                                                 /* takes all inputs and */
-}
-
-
-
-
-function handleNumberButtons() {
-    $('body').on('click', '.js-data-number', function(event) {
-
-    });
-}
-
-
-function setUpEventHandlers() {
-    handleNumberButtons();
-    handleOperationButtons();
-    handleEqualsButton();
-    handleDeleteButton();
-    handleAllClearButton();
-    handlePerviousOperandTextElement();
-    handleCurrentOperandTextElement();
-}
-
-function initialize() {
-    setUpEventHandlers();
-
-}
-
-initialize()
+const numberButtons = document.querySelectorAll('[data-number]')                            // First, select all elements that match a specfic string, use "[]"(bc data attribute) and selector.
+const operationButtons = document.querySelectorAll('[data-operation]')                           
+const equalsButtons = document.querySelectorAll('[data-equals]')                           
+const deleteButtons = document.querySelectorAll('[data-delete]')                           
+const allClearButton = document.querySelectorAll('[data-all-clear]')                           
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')                           
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')                           
