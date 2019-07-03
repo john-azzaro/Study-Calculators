8af53462eedf78c9800c9c79f1953be03c00effa
John Azzaro <jazzaro@temple.edu>
2019-07-02 21:18:18 -0700

    changed js selectors to data attributes

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 'use strict';
+
+class Calculator {                                              /* Calculator class to store our data input...*/
+                                                                 /* takes all inputs and */
+}
+
+
+
+
+function handleNumberButtons() {
+    $('body').on('click', '.js-data-number', function(event) {
+
+    });
+}
+
+
+function setUpEventHandlers() {
+    handleNumberButtons();
+    handleOperationButtons();
+    handleEqualsButton();
+    handleDeleteButton();
+    handleAllClearButton();
+    handlePerviousOperandTextElement();
+    handleCurrentOperandTextElement();
+}
+
+function initialize() {
+    setUpEventHandlers();
+
+}
+
+initialize()
