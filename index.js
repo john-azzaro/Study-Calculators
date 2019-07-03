dc1ac16d89220f0b9d64cdadd1a187148b931a0a
John Azzaro <jazzaro@temple.edu>
2019-07-03 13:05:06 -0700

    refactor coodeOperation, add additional compute

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  
 class Calculator {                                                               // Second, create a class and constructors that will take all the inputs and function for the calculator
    constructor(previousOperandTextElement, currentOperandTextElement) {          // so that we can set the text elements inside the calculator
-       this.previousOperandTextElement = previousOperandTextElement
-       this.currentOperandTextElement = currentOperandTextElement
+       this.previousOperandTextElement = previousOperandTextElement;
+       this.currentOperandTextElement = currentOperandTextElement;
        this.clear();                                                               // clear values when we create a new calculator.
    }                                                                                   
                                                                                 // Third, thinking of all the function the calculator can do and define them as a function below.
@@ -18,15 +18,23 @@ class Calculator {                                                              
    }
 
    appendNumber(number) {                                                           // every time a use clicks on a number, add to the screen. Also, pass the number the user selects.
-      if (number === '.' && this.currentOperand.includes('.')) {                         // resolves multiple decimal point issue... if the number is "." and already include it, 
-       return;                                                                           //  return
-      }                        
-      this.currentOperand = this.currentOperand.toString() + number.toString();           // update the currentOperand and append the current number by converting it to a string and append                                                                                         
-   }                                                                                    // with another number.  Note that you need to need to convert to strings is because javascript will
-                                                                                        // try to add as acutal numbers (i.e. 2 + 2 = 4 instead of 2 + 2 =) 
+        if (number === '.' && this.currentOperand.includes('.')) {                         // resolves multiple decimal point issue... if the number is "." and already include it, 
+        return;                                                                           //  return so the function will not execute any further.
+        }                        
+        this.currentOperand = this.currentOperand.toString() + number.toString();           // update the currentOperand and append the current number by converting it to a string and append                                                                                         
+   }                                                                                        // with another number.  Note that you need to need to convert to strings is because javascript will
+                                                                                            // try to add as acutal numbers (i.e. 2 + 2 = 4 instead of 2 + 2 =) 
 
    chooseOperation(operation) {                                                         // when the user clicks on an operation (i.e. +,-,*,%) and take the specific operation the user selected.
-
+        if (this.currentOperand === '') {                                                 // if the current operand is empty (i.e. ''), 
+            return;                                                                       //  return so the function will not execute any further.
+        }
+        if (this.previousOperand !== '') {                                                // if previous operand is not an empty string, then continue computation by...
+            this.compute();                                                               // .. call the compute function.
+        }                  
+        this.operation = operation;                                                     // set the operation to be equal to the operation passed in...
+        this.previousOperand = this.currentOperand;                                      // when the current number is done, push this into previousOperand...
+        this.currentOperand = '';                                                       // make currentOperand an empty string (clearing out the value).
    }
 
    compute() {                                                                           // take the values int he calculator and compute a single value.
@@ -34,7 +42,8 @@ class Calculator {                                                              
    }
 
    updateDisplay() {                                                                     // updates display inside the output.
-        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.currentOperandTextElement.innerText = this.currentOperand;                     // this shows the current operand from input.
+        this.previousOperandTextElement.innerText = this.previousOperand;                    // this shows the previous operand from input
    }
 }
 
@@ -43,13 +52,13 @@ class Calculator {                                                              
 
 
   // First, select all elements that match a specfic string, use "[]"(bc data attribute) and selector.
-  const numberButtons = document.querySelectorAll('[data-number]')
-  const operationButtons = document.querySelectorAll('[data-operation]')
-  const equalsButton = document.querySelector('[data-equals]')
-  const deleteButton = document.querySelector('[data-delete]')
-  const allClearButton = document.querySelector('[data-all-clear]')
-  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-  const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  const numberButtons = document.querySelectorAll('[data-number]');
+  const operationButtons = document.querySelectorAll('[data-operation]');
+  const equalsButton = document.querySelector('[data-equals]');
+  const deleteButton = document.querySelector('[data-delete]');
+  const allClearButton = document.querySelector('[data-all-clear]');
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+  const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
                        
 
@@ -63,8 +72,15 @@ const calculator = new Calculator(previousOperandTextElement, currentOperandText
 
 numberButtons.forEach(function (button) {                                                        // so select number button and for each (loop over all the buttons),
     button.addEventListener('click', function() {                                                 // and add an event listener so that when the user clicks, the calc will...
-        calculator.appendNumber(button.innerText)                                                // calculator.appendNumber with the inner text (i.e. 1, 2, 3, etc.)...
-        calculator.updateDisplay()                                                               // and lastly update the display so every time the user clicks a button, it shows in display.
+        calculator.appendNumber(button.innerText);                                                // calculator.appendNumber with the inner text (i.e. 1, 2, 3, etc.)...
+        calculator.updateDisplay();                                                               // and lastly update the display so every time the user clicks a button, it shows in display.
+    });
+});
+ 
+operationButtons.forEach(function (button) {                                                  // for the operation button event listener....                                       
+    button.addEventListener('click', function() {                                                 
+        calculator.chooseOperation(button.innerText)                                          // choose an operation (via the calculator class) and pass the text of that operation 
+        calculator.updateDisplay();                                                             // ... and update the display.   
     });
 });
 
